a3cb2b5f41a51adecf93f592f8176f852d13c8ac
Zarifa Ibrahimzada <zarifa.ibrahimzada04@gmail.com>
2025-03-25 20:20:56 -0400

    Added comments to explain the program

--- IsPalindrome/Program.cs
+++ IsPalindrome/Program.cs
@@ -1,5 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+/* 
+The code creates a console application where the user is prompted
+to enter a string. It defines a method called IsPalindrome to 
+check if the string is a palindrome. The method removes spaces 
+and converts the string to lowercase before comparing it to its
+reversed version. If the original string matches the reversed
+string, it prints that the string is a palindrome; otherwise,
+it indicates that the string is not a palindrome.
+*/
 
 string[] words =  {"racecar" ,"talented", "deified", "tent", "tenet"};
 
